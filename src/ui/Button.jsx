e05f6134649f6b98aa2995a0a8f90f20b0cfe336
Lubomir80 <Kutsiyl@gmail.com>
2023-09-19 10:54:13 +0200

    cartStyles, cartItem

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,23 +4,22 @@ import { Link } from 'react-router-dom';
 
 
 function Button({ children, disabled, onClick, to, type }) {
-   const className = `cursor-pointer
-bg-yellow-400 uppercase font-semibold
-sm:px-6 sm:py-4
-text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full
-hover:bg-yellow-300 transition-colors duration-300 focus:outline-none
-  focus:ring focus:reing-yellow-300 focus:ring-yellow-300 focus:ring-offset-2`
-
    const base = `cursor-pointer
                bg-yellow-400 uppercase font-semibold
                text-stone-800 inline-block tracking-wide rounded-full
                hover:bg-yellow-300 transition-colors duration-300 focus:outline-none
-               focus:ring focus:reing-yellow-300 focus:ring-yellow-300 focus:ring-offset-2`
+               focus:ring focus:ring-yellow-300 focus:ring-offset-2`
 
 
    const styles = {
       primary: base + ` py-3 px-4 sm:px-6 sm:py-4`,
-      small: base + ` py-2 px-4 sm:px-5 sm:py-2 text-xs`
+      small: base + ` py-2 px-4 sm:px-5 sm:py-2 text-xs`,
+      secondary: `cursor-pointer py-2.5 px-4 sm:px-6 sm:py-3.5
+      uppercase border-2 border-stone-300 font-semibold
+      text-stone-400 inline-block tracking-wide rounded-full
+      hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none
+      focus:bg-stone-300
+      focus:ring focus:ring-stone-200 focus:ring-offset-2`
    }
 
 
@@ -30,6 +29,7 @@ hover:bg-yellow-300 transition-colors duration-300 focus:outline-none
          {children}
       </Link>
    )
+
 
 
    return (
